7eff79d208868392b3820e0ed4ab4eddf76cee32
Thomas Heilmann <1016315+theilm@users.noreply.github.com>
2021-09-28 17:59:33 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,4 +3,11 @@
 $EM_CONF['json_reports'] = array (
     'title' => 'JSON Reports',
     'description' => 'Outputs reports as JSON for processing in monitoring or alerting systems.',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
 );
