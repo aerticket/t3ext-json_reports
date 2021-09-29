319fcf8f419c45b060d82b8de22f80bcf037041d
Thomas Heilmann <1016315+theilm@users.noreply.github.com>
2021-09-29 11:06:51 +0200

    Avoid use statements in ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,22 +1,14 @@
 <?php
 
-use Mindscreen\JsonReports\Output\Json;
-use Mindscreen\JsonReports\Output\Nagios;
-
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
-}
-
-// Access to the reports script via HTTP is only granted to the allowed IP addresses or address range
-if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['allowedIpAddresses'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['allowedIpAddresses'] = '';
 }
 
 if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['output'])) {
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['output'] = [];
 }
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['output']['json'] = Json::class;
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['output']['nagios'] = Nagios::class;
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['output']['json'] = \Mindscreen\JsonReports\Output\Json::class;
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['output']['nagios'] = \Mindscreen\JsonReports\Output\Nagios::class;
 
 // Define default report group that does not exclude any reports
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['groups']['default'] = [
